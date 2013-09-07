b83c1e8de8441db9f7ee0300b37c69bf6d184519
grxiao <1146791217@qq.com>
2013-09-07 19:26:34 +0800

    helloworld

---
+++ ts.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ts.cpp
+// Author      : guorui
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World! I am ¹ùî£ " << endl;
+	return 0;
+}
